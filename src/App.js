2a392e6138ad2e1c6ea593d8a94c6b9387b3001e
lee <sipossa@naver.com>
2023-03-21 13:44:49 +0900

    20220321

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./Layout";
+import CateList from "./shop/CateList";
+import Itm from "./shop/Itm";
 import List from "./shop/List";
 import ListAll from "./shop/ListAll";
 
@@ -20,13 +22,19 @@ const App = () => {
         getData();
     }, []);
 
-    console.log(shopData)
+    const originalItm = shopData.map(it => it.category);
+    const filterItm = originalItm.filter(Boolean);
+    const categoryItm = [...new Set(filterItm)];
+
+    console.log(categoryItm)
 
     return (
         <Routes>
-            <Route path="/" element={<Layout />}>
+            <Route path="/" element={<Layout categoryItm={categoryItm} />}>
                 <Route index element={<List />} />
-                <Route path="/all" element={<ListAll shopData={shopData} />} />
+                <Route path="all" element={<ListAll shopData={shopData} />} />
+                <Route path=":cate" element={<CateList shopData={shopData} />} />
+                <Route path="detail/:itm" element={<Itm shopData={shopData} />} />
             </Route>
         </Routes>
     )
